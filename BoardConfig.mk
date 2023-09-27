5cb62ec1c22161a99c0388352c0fc9379a3eecd2
Vhmit <viktor.9630@hotmail.com>
2023-09-27 15:29:17 -0300

    j4corelte: Inherit from msm8917-common
    * Yes, we are starting a common dt.
    
    Change-Id: Ie3af6fbfff27c486c2151c3087d7ff346d6d7ee8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,9 @@
 DEVICE_PATH := device/samsung/j4corelte
 TARGET_VENDOR := samsung
 
+# Inherit from msm8917-common
+-include device/samsung/msm8917-common/BoardConfigCommon.mk
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := j4corelte, j4coreltejx
 
